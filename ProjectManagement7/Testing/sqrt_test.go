dbd73bd619ff3073edbf5ebc4dd6646328e3d192
kaushaltheeG <kaushalsk09@gmail.com>
2023-03-04 22:41:10 -0800

    import testing lib

--- ProjectManagement7/Testing/sqrt_test.go
+++ ProjectManagement7/Testing/sqrt_test.go
@@ -1,7 +1,8 @@
 package sqrt
 
 import (
-	"testing"
+  "testing"
+  "github.com/stretchr/testify/require"
 )
 
 func almostEqual(v1, v2 float64) bool {
@@ -11,11 +12,14 @@ func almostEqual(v1, v2 float64) bool {
 func TestSimple(t *testing.T) {
 	val, err := Sqrt(2)
 
-	if err != nil {
-		t.Fatalf("error in calulation - %s", err)
-	}
+	
+	require.NoError(t,err)
+	// if err != nil {
+		// 	t.Fatalf("error in calulation - %s", err)
+	// }
 
-	if !almostEqual(val, 1.414214) {
-		t.Fatalf("bad value -%f", val)
-	}
+	require.InDelta(t, 1.414214, val, 0.001) //like the almost equal func
+	// if !almostEqual(val, 1.414214) {
+	// 	t.Fatalf("bad value -%f", val)
+	// }
 }
